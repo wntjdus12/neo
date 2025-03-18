63bb72b1254ba3fea725cb90a8fc523be1c60d1b
wntjdus12 <tjdusdl0811@naver.com>
2025-03-18 17:58:01 +0900

    st_sql

---
+++ db/st_grade.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `st_grade` (
+  `ST_ID` int(11) NOT NULL,
+  `Linux` int(11) DEFAULT NULL,
+  `DB` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ST_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+INSERT INTO `st_grade` VALUES (202501,90,80),(202502,70,95),(202503,80,65);
