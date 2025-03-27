7ea79bbbdc32776565d8bbe0a66e67a72ca947a2
wntjdus12 <tjdusdl0811@naver.com>
2025-03-27 12:07:43 +0900

    shard complete

--- mongodb/shard/config_shard.sh
+++ mongodb/shard/config_shard.sh
@@ -4,7 +4,7 @@
 systemctl stop mongod
 
 # stop shard process
-./stop_shard.shard
+./stop_shard.sh 
 export IP_TEMP=$(ip addr | grep enp0s3 | grep inet | cut -d " " -f6 | cut -d "/" -f1)
 echo $IP_TEMP
 
@@ -24,16 +24,16 @@ sleep 3s
 mongo $IP_TEMP:27019 < rs.init
 
 # Router Server
-touch /shard.data/logs/mongorouter.log
+touch /shard/data/logs/mongorouter.log
 
-mongos --config /shard/mongoRouter.conf &
+mongos --config /work/neo/mongodb/shard/mongoRouter.conf &
 sleep 3s
 
 # Shard1 Server
 mkdir -pv /shard/data/shard1db
 touch /shard/data/logs/shard1.log
 
-mongod --config /shard/mongoShard1.conf &
+mongod --config /work/neo/mongodb/shard/mongoShard1.conf &
 sleep 2s
 
 mongo $IP_TEMP:27021 < rs.init
@@ -42,7 +42,7 @@ mongo $IP_TEMP:27021 < rs.init
 mkdir -pv /shard/data/shard2db
 touch /shard/data/logs/shard2.log
 
-mongod --config /shard/mongoShard2.conf &
+mongod --config /work/neo/mongodb/shard/mongoShard2.conf &
 sleep 2s
 
 mongo $IP_TEMP:27022 < rs.init
@@ -50,7 +50,7 @@ mongo $IP_TEMP:27022 < rs.init
 # process status
 sleep 2s
 
-mongo $IP_TEMP:27017 < rs.assShard
+mongo $IP_TEMP:27017 < rs.addShard
 
-#netstatus
+# netstatus
 netstat -ntlp | grep mongo
