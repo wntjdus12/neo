4b4294e685303163cc1350f9de06490d46d61e7e
wntjdus12 <tjdusdl0811@naver.com>
2025-03-28 14:07:18 +0900

    oracle node web

---
+++ oracle/data_import.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo
+echo "## Database Data Import"
+sqlplus system/1234@xe < shop_user_create.sql
+sqlplus system/1234@xe < shop_membertbl.sql
+sqlplus system/1234@xe < shop_producttbl.sql
+sqlplus system/1234@xe < shop_usertbl.sql
+sqlplus system/1234@xe < shop_buytbl.sql
+sqlplus system/1234@xe < hr_usertbl.sql
+sqlplus system/1234@xe < hr_buytbl.sql
+echo
+echo "Database Data Import Done"
--- oracle/reinstall.sh
+++ oracle/reinstall.sh
@@ -60,3 +60,6 @@ EOF
 
 echo 
 echo "## setup Done"
+
+
+
