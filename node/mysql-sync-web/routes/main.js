8e0ff93214795e4222a60c1bd5878bd25176f3bb
wntjdus12 <tjdusdl0811@naver.com>
2025-03-28 17:09:27 +0900

    node mysql

---
+++ node/mysql-sync-web/routes/main.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('sync-mysql');
+const env = require('dotenv').config({ path: "../../.env"})
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+var connection = new mysql({
+    host: process.env.host,
+    user: process.env.user,
+    port: process.env.port,
+    password: process.env.password,
+    database: process.env.database
+})
+
+app.get('/Hello', (req, res) => {
+    res.send('Hello World!!')
+})
+
+
+
+app.get('/select', (req, res) => {
+    let result = connection.query('select * from st_info');
+    console.log(result);
+    res.send(result);
+})
+
+app.get('/insert', (req, res) => {
+    const { st_id, name, dept } = req.query;
+    let result = connection.query("insert into st_info values (?, ?, ?)", 
+        [st_id, name, dept]);
+        res.redirect('/select');
+})
+
+app.get('/update', (req, res) => {
+    const { st_id, name, dept } = req.query;
+    const result = connection.query("update st_info set NAME = ?, DEPT = ?  where ST_ID = ?",
+        [name, dept, st_id]);
+        res.redirect('/select');
+})
+
+app.get('/delete', (req, res) => {
+    const { st_id } = req.query;
+    const result = connection.query("delete from st_info where ST_ID = ?",
+        [st_id]);
+        res.redirect('/select');
+})
+
+module.exports = app;
+
--- node/mysql/app.js
+++ node/mysql/app.js
@@ -6,30 +6,31 @@ var app = express();
 var connection = mysql.createConnection({
     host: process.env.host,
     user: process.env.user,
-    password:process.env.password,
+    port: process.env.port,
+    password: process.env.password,
     database: process.env.database
 });
 
 connection.connect(function (err) {
-    if (err) {
-        console.error('Database is connected~!!\n\n');
+    if (!err) {
+        console.log("Database is connected~!!\n\n");
     } else {
-        console.log('Error connecting Database~!!\n\n')
+        console.log("Error connecting Database~!!")
     }
-});
+})
 
-app.get('/', function (req, res){
-    connection.query('SELECT * FROM st_info', function (err, rows, fields) {
+app.get('/', function (req, res) {
+    connection.query('select * from st_info', function (err, rows, fields) {
         connection.end();
-        if (err) {
-            res.send(rews);
-            console.log("The solution is : ",rows);
+        if (!err) {
+            res.send(rows);
+            console.log("The solution is : ", rows);
         } else {
-            console.log('Error while performing Query~!!\n\n')
+            console.log('Error while performing Query~!!\n');
         }
     })
 })
 
-app.listen(8000, function() {
-    console.log("8000 Port : Server Started~!!\n\n")
+app.listen(8000, function () {
+    console.log('8000 Port : Server Started~!!\n');
 })
