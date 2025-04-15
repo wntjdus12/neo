c192a6612537ca3ae9abaf0d9931b5c2ef1ac0ba
wntjdus12 <tjdusdl0811@naver.com>
2025-04-15 12:21:09 +0900

    치킨브랜드 크롤링

--- python/pythonData/p340_ChickenUtil.py
+++ python/pythonData/p340_ChickenUtil.py
@@ -1,3 +1,5 @@
+
+import time, datetime, ssl
 import pandas as pd
 import urllib.request
 
@@ -9,7 +11,7 @@ from bs4 import BeautifulSoup
 class ChickenStore():
     myencoding = 'utf-8'
 
-    def geWebDraiver(self, cmdJavaScript):
+    def getWebDraiver(self, cmdJavaScript):
         print(cmdJavaScript)
         self.driver.execute_script(cmdJavaScript)
         wait = 5
@@ -21,30 +23,29 @@ class ChickenStore():
     def getSoup(self):
         if self.soup == None:
             return None
-        else: 
-            if self.soup == None:
-                return None
-            else:
-                return BeautifulSoup(self.soup, 'html.parser')
+        else:
+            return BeautifulSoup(self.soup, 'html.parser')
+    
     def get_request_url(self):
         request = urllib.request.Request(self.url)
         try:
             context = ssl._create_unverified_context()
             response = urllib.request.urlopen(request, context=context)
             if response.getcode() == 200:
-                return response.read().decode(self.myencoding)
-            else:
-                return response
+                if self.brandName != 'pelicana':
+                    return response.read().decode(self.myencoding)
+                else:
+                    return response
         except Exception as err:
             print(err)
             now = datetime.datetime.now()
-            msg = '[%s] Error for URL : %s' % (now, self.url)
+            msg = '[%s] error for url : %s' % (now, self.url)
             print(msg)
             return None
         
     def save2Csv(self, result):
         data = pd.DataFrame(result, columns=self.mycolumns)
-        data.to_csv(self.brandName + '.csv', encoding=self.myencoding, mode='w', index=True)
+        data.to_csv(self.brandName + '.csv', encoding=self.myencoding, index=True)
 
     def __init__(self, brandName, url):
         self.brandName = brandName
@@ -53,10 +54,10 @@ class ChickenStore():
         self.mycolumns = ['brand', 'store', 'sido', 'gungu', 'address']
 
         if self.brandName in ['pelicana', 'nene', 'cheogajip', 'goobne']:
-            self.mycolums.append('phone')
+            self.mycolumns.append('phone')
         else:
             pass
-
+    
         if self.brandName != 'goobne':
             self.soup = self.get_request_url()
             self.driver = None
@@ -65,3 +66,4 @@ class ChickenStore():
             filepath = '/root/chromedriver/chromedriver'
             self.driver = webdriver.Chrome(filepath)
             self.driver.get(self.url)
+    
