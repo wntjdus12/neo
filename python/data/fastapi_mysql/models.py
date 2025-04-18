fcc40e7263daf2a7fd38897568fb7bcc82cadd0c
wntjdus12 <tjdusdl0811@naver.com>
2025-04-18 15:16:10 +0900

    festAPI

---
+++ python/data/fastapi_mysql/models.py
@@ -0,0 +1,19 @@
+
+from sqlalchemy import Column, TEXT, INT
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class St_info(Base):
+    __tablename__ = 'st_info'
+    
+    ST_ID = Column(INT, primary_key=True, nullable=False)
+    NAME = Column(TEXT, nullable=False)
+    DEPT = Column(TEXT, nullable=False)
+    
+class St_grade(Base):
+    __tablename__ = 'st_grade'
+    
+    ST_ID = Column(INT, primary_key=True, nullable=False)
+    Linux = Column(TEXT, nullable=False)
+    DB = Column(TEXT, nullable=False)
