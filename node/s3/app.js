3a2d6cc0dbf3255b89aa855734ab38ba65884e98
wntjdus12 <tjdusdl0811@naver.com>
2025-05-02 02:23:52 +0000

    s3

--- node/s3/app.js
+++ node/s3/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const bodyParser = require('body-parser');
-const cookieParser =  require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 app.set('port', process.env.Port || 8000);
+app.set('views', path.join(__dirname, 'public'));
+app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,5 +19,7 @@ var main = require('./routes/main');
 app.use('/', main);
 
 app.listen(app.get('port'), function () {
-    console.log("Server is Started~!! Port : " + app.get('port'));
-})
+    var dir = './uploadedFiles';
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+    console.log('Server is Started~!! Port : ' + app.get('port'));
+});
--- node/s3_list/routes/main.js
+++ node/s3_list/routes/main.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const fs = require('fs')
-const path = require('path')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
 const env = require('dotenv').config({ path: '../../.env' });
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended : false }))
-app.use(express.json())
-app.use(express.urlencoded({ extended : true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const AWS = require('aws-sdk');
 const ID = process.env.ID;
@@ -19,59 +19,14 @@ const s3 = new AWS.S3({ accessKeyId: ID, secretAccessKey: SECRET, region: MYREGI
 
 app.get('/list', (req, res) => {
     const params = {
-        Bucket : BUCKET_NAME,
-        Delimiter : '/',
-        Prefix : 'uploadedFiles/',
-    }
-    s3.listObjects(params, function(err, data) {
+        Bucket: BUCKET_NAME,
+        Delimiter: '/',
+        Prefix: 'uploadedFiles/',
+    };
+    s3.listObjects(params, function (err, data) {
         if (err) throw err;
-        // res.json(data.Contents)
-        res.writeHead(200);
-        var template = `
-            <!doctype html>
-            <html>
-            <head>
-                <title>Result</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <table border="1" margin: auto; text-align: center;>
-                    <tr>
-                        <th> Key </th>
-                        <th> LastModified </th>
-                        <th> Size </th>
-                        <th> StorageClass </th>
-                        <th> Down </th>
-                        <th> Del </th>
-                    </tr>
-        `;
-        for (var i = 1; i < data.Contents.length; i++) {
-          template += `
-                    <tr>
-                        <th> ${data.Contents[i]['Key']} </th>
-                        <th> ${data.Contents[i]['LastModified']} </th>
-                        <th> ${data.Contents[i]['Size']} </th>
-                        <th> ${data.Contents[i]['StorageClass']} </th>
-                        <th> 
-                            <form method='post' action='downloadFile'>
-                            <button type='submit' name='dlKey' value=${data.Contents[i]['Key']}>Down</button>
-                            </form>
-                        </th>
-                        <th> 
-                            <form method='post' action='deleteFile'>
-                            <button type='submit' name='dlKey' value=${data.Contents[i]['Key']}>Del</button>
-                            </form>
-                        </th>
-                    </tr>
-            `;
-        }
-        template += `
-                </table>
-            </body>
-            </html>
-            `;
-        res.end(template);
-      });
+        res.json(data.Contents);
     });
+});
 
 module.exports = app;
