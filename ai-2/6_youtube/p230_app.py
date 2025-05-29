70633fefb9ee449ffe82ac4b8c197d1115d10b56
wntjdus12 <164497167+wntjdus12@users.noreply.github.com>
2025-05-29 15:39:13 +0900

    test
    test

---
+++ ai-2/6_youtube/p230_app.py
@@ -0,0 +1,77 @@
+## p230_app.py
+
+import yt_dlp
+from pathlib import Path
+
+def get_youtube_video_info(video_url):
+    ydl_opts = {
+        'cookies' :'./data/cookies.txt',
+        'noplaylist': True,
+        'quiet': True,
+        'no_warnings': True
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            video_info = ydl.extract_info(video_url, download=False)
+            return {
+                'id' : video_info.get('id'),
+                'title' : video_info.get('title'),
+                'upload_date': video_info.get('upload_date'),
+                'channel' : video_info.get('channel'),
+                'duration' : video_info.get('duration_string'),
+            }
+    except Exception as e:
+        return f'Error: {e}'
+
+def remove_invalid_char_for_filename(input_str):
+    invalid_characters = '<>:"/\|?*'
+
+    for char in invalid_characters:
+        input_str = input_str.replace(char, '_')
+
+    while input_str.endswith('.'):
+        input_str = input_str[:-1]
+
+    return input_str
+
+def download_youtube_video(video_url, folder, filename=None):
+    video_info = get_youtube_video_info(video_url)
+
+    if isinstance(video_info, str) and video_info.startswith("Error"):
+        print(video_info)
+        return None, None
+
+    title = video_info['title']
+    filename_no_ext = remove_invalid_char_for_filename(title)
+
+    if filename is None:
+        download_file = f"{filename_no_ext}.mp4"
+    else:
+        download_file = filename
+
+    outtmpl_str = f'{folder}/{download_file}'
+
+    download_path = Path(outtmpl_str)
+
+    ydl_opts = {
+        'cookies' :'./data/cookies.txt',
+        'format': 'best',
+        'outtmpl': outtmpl_str,
+        'noplaylist': True,
+        'quiet': True,
+        'no_warnings': True
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        video_info = ydl.extract_info(video_url, download=True)
+        title = video_info.get('title', None)
+
+    return title, download_path
+
+video_url = 'https://www.youtube.com/watch?v=pSJrML-TTmI'
+download_folder = './data'
+video_title, download_path = download_youtube_video(video_url, download_folder)
+
+print("- 유튜브 제목 : ", video_title)
+print("- 다운로드한 파일명 : ", download_path.name)
